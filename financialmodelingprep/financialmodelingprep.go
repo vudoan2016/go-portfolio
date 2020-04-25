90b393ed2ff2c1f629126afb4901294da5de8c86
vudoan2016 <vudoant@gmail.com>
2020-04-25 12:35:39 -0700

    Added financialmodelingprep module

---
+++ financialmodelingprep/financialmodelingprep.go
@@ -0,0 +1,44 @@
+package financialmodelingprep
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+const (
+	profileURL = "https://financialmodelingprep.com/api/v3/company/profile/"
+	ratingURL  = "https://financialmodelingprep.com/api/v3/company/rating/AAPL"
+)
+
+type Company struct {
+	P profile `json:"profile"`
+}
+
+type profile struct {
+	Industry string `json:"industry"`
+	Sector   string `json:"sector"`
+}
+
+// GetProfile returns key data of a company
+func GetProfile(symbol string) Company {
+	response, err := http.Get(profileURL + symbol)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var c Company
+	err = json.Unmarshal(responseData, &c)
+	if err != nil {
+		log.Println("Failed to unmarshal response")
+	}
+
+	return c
+}
